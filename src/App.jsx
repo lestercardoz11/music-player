37806948a393ebf56b685130610e936fc2a28f0e
lestercardoz11 <lestercardoz11@gmail.com>
2020-07-30 01:07:50 +0530

    Working API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import { FormGroup, FormControl, InputGroup } from 'react-bootstrap';
+import { FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
+    search() {
+        console.log('this.state', this.state);
+        const BASE_URL = 'https://spotify-api-wrapper.appspot.com/';
+        const FETCH_URL = `${BASE_URL}artist/:${this.state.query}`;
+        console.log('FETCH_URL', FETCH_URL);
+        fetch(FETCH_URL, {
+            method:'GET',
+            headers: new Headers({
+            'Authorization': 'Bearer BQBLsYmIh9...0'
+            }),
+            })
+            .then(response => response.json())
+            .then(json => console.log('json', json))
+            .catch(error => console.log('errore', error))
+    }
+
     render() {
         return(
             <div className="App">
@@ -12,10 +35,15 @@ class App extends Component {
                     <FormControl 
                         type="text"
                         placeholder="Artist"
+                        value={this.state.query}
+                        onChange={event => {this.setState({query: event.target.value})}}
+                        onKeyPress={ event => {
+                            if (event.key === 'Enter') {
+                                this.search()
+                            }
+                        }}
                     />
-                    <InputGroup.Addon>
-                        <span>Search</span>
-                    </InputGroup.Addon>
+                    <Button onClick={() => this.search()}>Search</Button>
                     </InputGroup>
                 </FormGroup>
                 <div className="Profile">
@@ -29,4 +57,5 @@ class App extends Component {
         )
     }
 }
+
 export default App;
